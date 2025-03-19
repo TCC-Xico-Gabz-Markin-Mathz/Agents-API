70da879f613c70232fbbe57c51dbf5c7ca36a8ae
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-03-19 10:49:08 -0300

    fix: payload info and route request/response

--- models/payloadRAG.py
+++ models/payloadRAG.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 class RAGQueryRequest(BaseModel):
     query: str
     database_structure: str
-    process: str
 
 class RAGQueryResponse(BaseModel):
-    optimized_query: str
+    query: str
