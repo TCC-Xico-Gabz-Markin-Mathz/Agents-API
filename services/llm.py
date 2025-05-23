50624d61c4609fbf4a3b2466d3d2c340f0825139
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-05-23 17:28:21 -0300

    feat: remove sentence transformers

--- services/llm.py
+++ services/llm.py
@@ -1,35 +1,9 @@
 from fastapi import HTTPException
 from typing import List
-from sentence_transformers import SentenceTransformer
 
 from services.db import connect
 from services.groq import llm_connect
 from models.llmModel import ContextOut
-
-model = SentenceTransformer('all-mpnet-base-v2')
-
-def transform_vector(vector: str) -> List[float]:
-    return model.encode([vector])[0]
-
-async def retrieve_context(query: str) -> List[ContextOut]:
-    contexts: List[ContextOut] = []
-    query_vector = transform_vector(query)
-    
-    try:
-        client = connect() 
-        results = client.search(
-            collection_name="rag_queries",
-            query_vector=query_vector.tolist(),
-            limit=2
-        )
-        
-        for result in results:
-            contexts.append(ContextOut(id=result.id, score=result.score, payload=result.payload))
-        
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao recuperar contexto: {str(e)}")
-    
-    return contexts
 
 async def get_sql_query_with_database_structure(database_structure: str, order: str) -> str:
     client = llm_connect()
