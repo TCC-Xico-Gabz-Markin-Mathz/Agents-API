5b61022a411958cb1c285dfd49d8812eaf48bc1f
Gabriel Siqueira <gabrielss2406@gmail.com>
2025-08-26 17:40:21 -0300

    debug

--- services/llmModels/openRouter/hermes.py
+++ services/llmModels/openRouter/hermes.py
@@ -7,4 +7,5 @@ class HermesLLM(OpenRouterBaseLLMService):
         model_name = os.getenv(
             "HERMES_MODEL_NAME", "nousresearch/deephermes-3-llama-3-8b-preview:free"
         )
+        print(f"Conectando ao modelo: {model_name}")
         super().__init__(model_name=model_name)
